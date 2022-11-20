4fee509e3bc451268ff8c2d578d21889019ef895
Chris <chris96.villarreal@gmail.com>
2022-11-20 17:07:31 -0600

    Register Customer and Payments to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, PaymentsCustomer
+
+admin.site.register(Customer)
+admin.site.register(PaymentsCustomer)
