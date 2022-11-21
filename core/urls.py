f6f343a4c5e541df2681a16beaae96ba04661c05
Chris <chris96.villarreal@gmail.com>
2022-11-20 22:59:57 -0600

    Create url for customer detail view

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, CustomerDetailView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('customer/<uuid:pk>', CustomerDetailView.as_view(), name='customer-detail'),
 ]
